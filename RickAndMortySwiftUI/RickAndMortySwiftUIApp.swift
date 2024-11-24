2ed850b93b2d0f1ece73477cb237331dabf7e570
János Székely <szekelyjani@gmail.com>
2024-11-24 17:19:48 +0100

    Initial Commit

---
+++ RickAndMortySwiftUI/RickAndMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySwiftUIApp.swift
+//  RickAndMortySwiftUI
+//
+//  Created by Szekely Janos on 2024. 11. 24..
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
